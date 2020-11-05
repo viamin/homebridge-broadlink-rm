d461f9e21063e69664852138f81e668ed539a36e
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-11-06 08:24:10 +1300

    Create fanv1.js

---
+++ accessories/fanv1.js
@@ -0,0 +1,206 @@
+const ServiceManagerTypes = require('../helpers/serviceManagerTypes');
+const SwitchAccessory = require('./switch');
+const catchDelayCancelError = require('../helpers/catchDelayCancelError');
+const ping = require('../helpers/ping');
+const delayForDuration = require('../helpers/delayForDuration');
+
+class FanAccessory extends SwitchAccessory {
+  setDefaults () {
+    super.setDefaults();
+    let { config, state } = this;
+    
+    // Defaults
+    config.showSwingMode = config.hideSwingMode === true || config.showSwingMode === false ? false : true;
+    config.showRotationDirection = config.hideRotationDirection === true || config.showRotationDirection === false ? false : true;
+    config.stepSize = isNaN(config.stepSize) || config.stepSize > 100 || config.stepSize < 1 ? 1 : config.stepSize
+    
+    if (config.alwaysResetToDefaults) {
+      state.fanSpeed = (config.defaultFanSpeed !== undefined) ? config.defaultFanSpeed : 100;
+    }
+  }
+
+  reset() {
+    super.reset();
+
+    this.stateChangeInProgress = true;
+    
+    // Clear Timeouts
+    if (this.delayTimeoutPromise) {
+      this.delayTimeoutPromise.cancel();
+      this.delayTimeoutPromise = null;
+    }
+
+    if (this.autoOffTimeoutPromise) {
+      this.autoOffTimeoutPromise.cancel();
+      this.autoOffTimeoutPromise = null;
+    }
+
+    if (this.autoOnTimeoutPromise) {
+      this.autoOnTimeoutPromise.cancel();
+      this.autoOnTimeoutPromise = null;
+    }
+  }
+  
+  checkAutoOnOff() {
+    this.reset();
+    this.checkAutoOn();
+    this.checkAutoOff();
+  }
+
+  async checkAutoOff() {
+    await catchDelayCancelError(async () => {
+      const { config, log, name, state, serviceManager } = this;
+      let { disableAutomaticOff, enableAutoOff, onDuration } = config;
+
+      if (state.switchState && enableAutoOff) {
+        log(
+          `${name} setSwitchState: (automatically turn off in ${onDuration} seconds)`
+        );
+
+        this.autoOffTimeoutPromise = delayForDuration(onDuration);
+        await this.autoOffTimeoutPromise;
+
+        serviceManager.setCharacteristic(Characteristic.On, false);
+      }
+    });
+  }
+
+  async checkAutoOn() {
+    await catchDelayCancelError(async () => {
+      const { config, log, name, state, serviceManager } = this;
+      let { disableAutomaticOn, enableAutoOn, offDuration } = config;
+
+      if (!state.switchState && enableAutoOn) {
+        log(
+          `${name} setSwitchState: (automatically turn on in ${offDuration} seconds)`
+        );
+
+        this.autoOnTimeoutPromise = delayForDuration(offDuration);
+        await this.autoOnTimeoutPromise;
+
+        serviceManager.setCharacteristic(Characteristic.On, true);
+      }
+    });
+  }
+
+  async setSwitchState (hexData, previousValue) {	  
+    const { config, state, serviceManager } = this;
+    if (!this.state.switchState) {
+      this.lastFanSpeed = undefined;
+    }
+	  
+    // Reset the fan speed back to the default speed when turned off
+    if (this.state.switchState === false && config.alwaysResetToDefaults) {
+      this.setDefaults();
+      serviceManager.setCharacteristic(Characteristic.RotationSpeed, state.fanSpeed);
+    }
+
+    super.setSwitchState(hexData, previousValue);
+  }
+  
+  async setFanSpeed (hexData) {
+    const { data, host, log, state, name, debug} = this;
+
+    this.reset();
+
+    // Create an array of speeds specified in the data config
+    const foundSpeeds = [];
+    const allHexKeys = Object.keys(data || {});
+
+    allHexKeys.forEach((key) => {
+      const parts = key.split('fanSpeed');
+
+      if (parts.length !== 2) return;
+
+      foundSpeeds.push(parts[1])
+    })
+
+    if (foundSpeeds.length === 0) {
+
+      return log(`${name} setFanSpeed: No fan speed hex codes provided.`)
+    }
+
+    // Find speed closest to the one requested
+    const closest = foundSpeeds.reduce((prev, curr) => Math.abs(curr - state.fanSpeed) < Math.abs(prev - state.fanSpeed) ? curr : prev);
+    log(`${name} setFanSpeed: (closest: ${closest})`);
+
+    if (this.lastFanSpeed === closest) {
+      return;
+    }
+
+    this.lastFanSpeed = closest;
+
+    // Get the closest speed's hex data
+    hexData = data[`fanSpeed${closest}`];
+
+    await this.performSend(hexData);
+
+    this.checkAutoOnOff();
+  }
+
+  setupServiceManager () {
+    const { config, data, name, serviceManagerType } = this;
+    const { on, off, clockwise, counterClockwise, swingToggle } = data || {};
+
+    this.setDefaults();
+
+    this.serviceManager = new ServiceManagerTypes[serviceManagerType](name, Service.Fan, this.log);
+
+    this.serviceManager.addToggleCharacteristic({
+      name: 'switchState',
+      type: Characteristic.On,
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        onData: on,
+        offData: off,
+        setValuePromise: this.setSwitchState.bind(this)
+      }
+    });
+
+    if (config.showSwingMode) {
+      this.serviceManager.addToggleCharacteristic({
+        name: 'swingMode',
+        type: Characteristic.SwingMode,
+        getMethod: this.getCharacteristicValue,
+        setMethod: this.setCharacteristicValue,
+        bind: this,
+        props: {
+          onData: swingToggle,
+          offData: swingToggle,
+        }
+      });
+    }
+
+    this.serviceManager.addToggleCharacteristic({
+      name: 'fanSpeed',
+      type: Characteristic.RotationSpeed,
+      getMethod: this.getCharacteristicValue,
+      setMethod: this.setCharacteristicValue,
+      bind: this,
+      props: {
+        setValuePromise: this.setFanSpeed.bind(this),
+		    minStep: config.stepSize,
+		    minValue: 0,
+		    maxVlue: 100
+      }
+    });
+
+    if (config.showRotationDirection) {
+      this.serviceManager.addToggleCharacteristic({
+        name: 'rotationDirection',
+        type: Characteristic.RotationDirection,
+        getMethod: this.getCharacteristicValue,
+        setMethod: this.setCharacteristicValue,
+        bind: this,
+        props: {
+          onData: counterClockwise,
+          offData: clockwise
+        }
+      });
+    }
+  }
+}
+
+module.exports = FanAccessory;
