76eaeb033a1a4de31b498d862f88878c5927cf1c
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-12-08 12:44:36 +1300

    Update temperatureSensor.js

--- accessories/temperatureSensor.js
+++ accessories/temperatureSensor.js
@@ -45,7 +45,7 @@ class TemperatureSensorAccessory extends AirconAccessory {
     const { config, state, log, debug } = this;
 
     const batteryAlert = state.batteryLevel <= 20? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-    if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:',state.batteryLevel,'Alert:',batteryAlert);
+    if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:`,state.batteryLevel,'Alert:',batteryAlert);
 
     callback(null, batteryAlert);
   }
