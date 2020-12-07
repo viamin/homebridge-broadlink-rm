638d6f942bf818790a2736dfb7155c2685353ab7
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-12-08 12:50:54 +1300

    Added Battery Level

--- accessories/temperatureSensor.js
+++ accessories/temperatureSensor.js
@@ -45,9 +45,15 @@ class TemperatureSensorAccessory extends AirconAccessory {
     const { config, state, log, debug } = this;
 
     const batteryAlert = state.batteryLevel <= 20? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-    if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:`,state.batteryLevel,'Alert:',batteryAlert);
+    if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Alert:`,batteryAlert);
 
     callback(null, batteryAlert);
+  }
+  
+  getBatteryLevel (callback) {
+    const { config, state, log, debug } = this;
+    if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:`,state.batteryLevel);
+    callback(null, parseFloat(state.batteryLevel));
   }
   
   // Service Manager Setup
@@ -78,6 +84,13 @@ class TemperatureSensorAccessory extends AirconAccessory {
         name: 'batteryAlert',
         type: Characteristic.StatusLowBattery,
         method: this.getBatteryAlert,
+        bind: this
+      })
+      
+      this.serviceManager.addGetCharacteristic({
+        name: 'batteryLevel',
+        type: Characteristic.BatteryLevel,
+        method: this.getBatteryLevel,
         bind: this
       })
     };
