b3d1c24ab641ae184744a157f063427996ba6a66
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2020-12-09 14:23:52 +1300

    Fixed name not defined error

--- accessories/humiditySensor.js
+++ accessories/humiditySensor.js
@@ -56,7 +56,7 @@ class HumiditySensorAccessory extends HumidifierAccessory {
   async updateDeviceState () { return;}
   
   getBatteryAlert (callback) {
-    const { config, state, log, debug } = this;
+    const { config, name, state, log, debug } = this;
 
     const batteryAlert = state.batteryLevel <= 20? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
     if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:`,state.batteryLevel,'Alert:',batteryAlert);
@@ -65,7 +65,7 @@ class HumiditySensorAccessory extends HumidifierAccessory {
   }
   
   getBatteryLevel (callback) {
-    const { config, state, log, debug } = this;
+    const { config, name, state, log, debug } = this;
     if (debug) log(`\x1b[34m[DEBUG]\x1b[0m ${name} Battery Level:`,state.batteryLevel);
     callback(null, parseFloat(state.batteryLevel));
   }
